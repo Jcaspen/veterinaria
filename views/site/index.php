657d478191fc0eb50607f5273978edf1eae0569a
Jose Luis Castillo peña <jcaspen75@gmail.com>
2021-02-12 15:30:53 +0300

    Modificación de pantalla inicial

--- views/site/index.php
+++ views/site/index.php
@@ -7,11 +7,11 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>SERVAIS VETERINARIA</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">¡Mirando por el futuro de tu mascota!</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Pedir Cita</a></p>
     </div>
 
     <div class="body-content">
@@ -25,7 +25,7 @@ $this->title = 'My Yii Application';
                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                     fugiat nulla pariatur.</p>
 
-                <p><a class="btn btn-outline-info" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <p><a class="btn btn-outline-info" href="http://www.yiiframework.com/doc/">Curar Mascota Virtual</a></p>
             </div>
             <div class="col-xl-4">
                 <h2>Heading</h2>
@@ -35,7 +35,7 @@ $this->title = 'My Yii Application';
                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                     fugiat nulla pariatur.</p>
 
-                <p><a class="btn btn-outline-info" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+                <p><a class="btn btn-outline-info" href="http://www.yiiframework.com/forum/">Hoss Denay</a></p>
             </div>
             <div class="col-xl-4">
                 <h2>Heading</h2>
@@ -45,7 +45,7 @@ $this->title = 'My Yii Application';
                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                     fugiat nulla pariatur.</p>
 
-                <p><a class="btn btn-outline-info" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                <p><a class="btn btn-outline-info" href="http://www.yiiframework.com/extensions/">Sudo Servais</a></p>
             </div>
         </div>
 
